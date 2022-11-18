c79b8030e5d41790d820ad320708ec851407cd5e
connetium-fatih <fatih16bilgin@gmail.com>
2022-11-18 14:53:50 +0300

    first commit

---
+++ Taygır/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taygır
+//
+//  Created by Fatih Bilgin on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
